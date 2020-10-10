a8238c4a319f373322bdcf936a28d6dd5f245d22
dhall2211 <dhall2211@gmail.com>
2020-10-10 13:11:51 +0200

    initial commit

---
+++ src/test.java
@@ -0,0 +1,18 @@
+import javax.print.DocFlavor;
+
+public class Test {
+
+    public static  void testMain (String [] arg) {
+
+        testNumber(5);
+        this.testN();
+
+    }
+    public static int testNumber (int num) {
+        return num +1;
+    }
+    public int testN (int num) {
+        return num +2;
+
+    }
+}
